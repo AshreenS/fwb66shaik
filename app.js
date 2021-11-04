9b447460480e2e8102a766d80113a473d4ed5fbf
AshreenS <shaikashreen@gmail.com>
2021-11-03 21:09:03 -0500

    updated the files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gucciRouter = require('./routes/gucci');
+var addmodRouter = require('./routes/addmod');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gucci', gucciRouter);
+app.use('/addmod', addmodRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
